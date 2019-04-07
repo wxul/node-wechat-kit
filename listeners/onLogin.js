4ce6b41c6b6a2076cac41d24e5ca11ec9ce8daed
albert <wlj_3322@163.com>
2019-04-07 19:50:50 +0800

    更改定时逻辑

--- listeners/onLogin.js
+++ listeners/onLogin.js
@@ -3,17 +3,27 @@ const {
 } = require('wechaty');
 const schedule = require('node-schedule');
 const rss = require('../service/zhihurss');
+const admins = require('../admins');
 
 async function onLogin(user) {
     log.info(`${user} login`);
 
-    schedule.scheduleJob('* 10 * * *', async function () {
+    schedule.scheduleJob('0 0 10 * * *', async function () {
         try {
-            let rooms = await bot.Room.findAll('');
-            let l = await rss();
-            for (var k in rooms) {
-                await rooms[k].say(l);
+            let senders = [];
+            // let senders = await bot.Room.findAll('');
+            for (var k in admins) {
+                let find = await bot.Contact.find({
+                    name: admins[k]
+                });
+                // console.log('find user:', find);
+                find && senders.push(find);
             }
+            let l = await rss();
+            for (var k in senders) {
+                await senders[k].say(l);
+            }
+
         } catch (error) {
 
         }
