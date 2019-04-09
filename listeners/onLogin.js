220f8c086cd70f9d65a4278aba8f401714f35962
Wuliangjie <wlj_3322@163.com>
2019-04-09 10:36:13 +0800

    改用群发

--- listeners/onLogin.js
+++ listeners/onLogin.js
@@ -10,15 +10,15 @@ async function onLogin(user) {
 
     schedule.scheduleJob('0 0 10 * * *', async function () {
         try {
-            let senders = [];
-            // let senders = await bot.Room.findAll('');
-            for (var k in admins) {
-                let find = await bot.Contact.find({
-                    name: admins[k]
-                });
-                // console.log('find user:', find);
-                find && senders.push(find);
-            }
+            // let senders = [];
+            let senders = await bot.Room.findAll('');
+            // for (var k in admins) {
+            //     let find = await bot.Contact.find({
+            //         name: admins[k]
+            //     });
+            //     // console.log('find user:', find);
+            //     find && senders.push(find);
+            // }
             let l = await rss();
             for (var k in senders) {
                 await senders[k].say(l);
